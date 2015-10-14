9d132636a920cf6fd38078bbdaf7beb906b50ab0
Lauri <lauri.meriloo@gmail.com>
2015-10-14 22:26:29 +0300

    * MovieType UNKNOWN added

--- src/main/java/ee/meriloo/items/MovieType.java
+++ src/main/java/ee/meriloo/items/MovieType.java
@@ -1,12 +1,15 @@
 package ee.meriloo.items;
 
+import org.omg.CORBA.UNKNOWN;
+
 /**
  * Created by Lauri on 09.10.2015.
  */
 public enum MovieType {
     NEW_RELEASE("New release"),
     REGULAR("Regular rental"),
-    OLD_FILM("Old film");
+    OLD_FILM("Old film"),
+    UNKNOWN("N/A");
 
     String type;
 
